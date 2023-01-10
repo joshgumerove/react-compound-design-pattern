4973e5c1cfab49415e27b05973d63f7953b5cbe9
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-01-10 15:17:47 -0500

    add styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
+import React, { useState } from "react";
 import "./App.css";
 import Tab from "./Tab";
 
 function App() {
+  const [currentTabIndex, setCurrentTabIndex] = useState(0);
+
+  const handleChange = (newIndex) => {
+    setCurrentTabIndex(newIndex);
+  };
   return (
     <div className="App">
       <h1>Compound Pattern Design</h1>
-      <Tab>
+      <Tab currentTab={currentTabIndex} onChange={handleChange}>
         <Tab.HeadsContainer>
           <Tab.Item label="Tab1" index={0} />
           <Tab.Item label="Tab2" index={1} />
           <Tab.Item label="Tab3" index={2} />
         </Tab.HeadsContainer>
+        <Tab.ContentContainer>
+          <Tab.ContentItem index={0}>
+            <h1>I am content</h1>
+          </Tab.ContentItem>
+          <Tab.ContentItem index={1}>
+            <h1>I am content</h1>
+          </Tab.ContentItem>
+          <Tab.ContentItem index={2}>
+            <h1>I am content</h1>
+          </Tab.ContentItem>
+        </Tab.ContentContainer>
       </Tab>
       {/* text is still considered a child */}
     </div>
--- src/Tab.js
+++ src/Tab.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./Tab.css";
 
 const TabContext = React.createContext();
 
 const Tab = ({ currentTab, onChange, children }) => {
   return (
-    <div>
+    <div className="tab">
       <TabContext.Provider value={{ currentTab, onChange }}>
         {children}
       </TabContext.Provider>
@@ -13,14 +14,18 @@ const Tab = ({ currentTab, onChange, children }) => {
 };
 
 Tab.HeadsContainer = ({ children }) => {
-  return <div>{children}</div>;
+  return <div className="headsContainer">{children}</div>;
 };
 
 Tab.Item = ({ label, index, children }) => {
-  return <div>{label}</div>;
+  return <div className="item">{label}</div>;
 };
 
 Tab.ContentContainer = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+Tab.ContentItem = ({ index, children }) => {
   return <div>{children}</div>;
 };
 
